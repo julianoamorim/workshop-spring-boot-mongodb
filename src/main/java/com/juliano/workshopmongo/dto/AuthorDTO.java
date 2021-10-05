3759375aa3961b1ef1ad173bc6f278780fcc108e
Juliano Amorim <julianoamorim89@outlook.com>
2021-10-05 20:26:04 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/juliano/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,33 @@
+package com.juliano.workshopmongo.dto;
+
+import com.juliano.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L; //permite q o objeto seja convertido em byte e trafegado em rede
+
+    private String id;
+    private String nome;
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
