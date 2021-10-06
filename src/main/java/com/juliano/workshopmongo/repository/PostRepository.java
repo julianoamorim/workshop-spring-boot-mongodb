eeaaa4c4dad8e9d754b068025af83c5ea3d6630c
Juliano Amorim <julianoamorim89@outlook.com>
2021-10-06 19:57:54 -0300

    Consulta simples com @Query

--- src/main/java/com/juliano/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/juliano/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.juliano.workshopmongo.repository;
 
 import com.juliano.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { //Repository Mongo gerencia a classe User com id tipo String
 
-    List<Post> findByTitleContainingIgnoreCase(String text); //Query Method MongoDB, metodo de pesquisa automatico
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") //query manual do MongoDB, ver documentacao
+    List<Post> buscarTitulo(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text); //Query Method MongoDB, metodo de pesquisa automatico, ver documentacao
 }
